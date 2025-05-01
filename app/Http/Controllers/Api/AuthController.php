f6a4441872c9cdba86d1fbdef7a1a194ad132aba
Amer Oniza <ameroniza@gmail.com>
2025-05-01 17:28:07 +0300

    Add dashboadrd & API sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\CreateUserRequest;
+use App\Http\Requests\Api\LoginRequest;
+use App\Http\Resources\UserResource;
+use App\Services\AuthApiService ;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    protected AuthApiService $authService;
+
+    public function __construct(AuthApiService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function register(CreateUserRequest $request)
+    {
+        $data = $request->validated();
+        [$user, $token] = $this->authService->register($data);
+
+        return $this->registerResponse(new UserResource($user),$token);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+        [$user, $token] = $this->authService->login($data);
+
+        return $this->loginResponse(new UserResource($user), $token);
+    }
+
+    public function user(Request $request)
+    {
+        return $this->successResponse('User retrieved successfully', new UserResource($request->user()));
+    }
+
+    public function logout(Request $request)
+    {
+        $this->authService->logout($request->user());
+
+        return $this->logoutResponse();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::controller(AuthController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login'   , 'login');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(AuthController::class)->group(function(){
+        Route::get('user', 'user');
+        Route::post('logout','logout');
+    });
+});
